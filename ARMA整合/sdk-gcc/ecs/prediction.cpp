aa6b0c872f7c851b935cb865375eb1b9ce4ed510
Citrix <sijieqian@foxmail.com>
2018-04-09 21:30:32 +0800

    改成动态预测，修正个别小问题，不加缺省得分77.407，不过加上缺省代码后，结果变差，需要重新调参

--- ARMA整合/sdk-gcc/ecs/prediction.cpp
+++ ARMA整合/sdk-gcc/ecs/prediction.cpp
@@ -9,28 +9,31 @@ void PredictAll(std::vector<std::vector<double> >& AlldataArray, int period, int
     int cnt = 5;//预测一天时选取最优的cnt个模型进行预测，再取平均。
     for(int n = 0; n < numFla; n++){//最大循环，共numfla个服务器
         //第n个服务器的差分预处理
+        int weekPredict = 0;
         std::vector<double> dataArray;
         dataArray.assign(AlldataArray[n].begin(), AlldataArray[n].end());
-        int diffGap = 7;
-        std::vector<double> arrayDiff(preDiff(dataArray, diffGap));
-
+        int diffGap = 7;//差分周期   
         std::vector<double> predictALL(period);//period天的预测值
-
-        for(int i = 0; i < period; i++){
-            //每次预测一天，共预测7次
+        std::vector<double> tempdataArray;//去掉原始数据末尾N位
+        //设置缺省时间段
+        int dataArrayNums = dataArray.size();
+        int aheaddays = dataArrayNums - 7;
+        //将去掉尾部的数据赋值给tempdataArray
+        for (int i = 0; i < aheaddays ; i++)
+        {
+            tempdataArray.push_back(dataArray[i]);
+            std::cout<<dataArray[i]<<std::endl;
+        } 
+        //预测的天数改为period加上14天
+        for(int i = 0; i < period+7; i++){
             std::vector<std::vector<int>> usedPQ;//存放使用过的p、q对
             std::vector<double> bestPredict(cnt);//存放最优的预测值
+            std::vector<double> arrayDiff(preDiff(tempdataArray, diffGap));//生成差分序列
             double predictAvg = 0.0;
 
             for(int j = 0; j < cnt; j ++) {
                 int maxP = (int) std::log(arrayDiff.size());
-//                int maxP = (int)pow(round(12 * arrayDiff.size() / 100.0), 0.25);
-//                if(maxP > arrayDiff.size() - 1){
-//                    maxP = arrayDiff.size() -1;
-//                }
-                int maxQ = 0;
-//                int maxP = 7;
-//                int maxQ = 7;
+                int maxQ = 7;
 
                 std::vector<int> bestModel(predictByAIC(arrayDiff, maxP, maxQ, usedPQ,j));
 
@@ -41,50 +44,46 @@ void PredictAll(std::vector<std::vector<double> >& AlldataArray, int period, int
                 PQ[1] = bestQ;
                 usedPQ.push_back(PQ);
 
-                //        int bestP = (int)pow(round(12 * arrayDiff.size() / 100.0), 0.25);
-                //        if(bestP > arrayDiff.size() - 1){
-                //            bestP = arrayDiff.size() -1;
-                //        }
-                //        int bestQ = 0;
-
                 ARMA arma = ARMA(arrayDiff, bestP, bestQ);
                 arma.calculateARMACoe();
-
-//                AR arma = AR(arrayDiff, bestP);
-//                arma.calculateCoe();
 
                 std::vector<double> predictOneDay(arma.predict(1));
                 bestPredict[j] = predictOneDay[0];
             }
+
             for(int k = 0; k < bestPredict.size(); k++){
                 predictAvg += bestPredict[k];
             }
-            predictAvg = predictAvg/bestPredict.size();
-
-            predictALL[i] = predictAvg;
-
-            arrayDiff.push_back(predictAvg);
-        }
-
-        std::vector<double> arrayForDiffReverse;
-        for(int i = diffGap - 1; i >= 0; i--){
-            arrayForDiffReverse.push_back(dataArray[dataArray.size() - i]);
-        }
-        for(int i = 0; i < predictALL.size(); i++){
-            arrayForDiffReverse.push_back(predictALL[i]);
-        }
-
-        std::vector<double> predictDiffReverse(contrastDiff(arrayForDiffReverse, diffGap));
-
-
-        for(int i = 0; i < predictDiffReverse.size(); i++){
-//            std::cout<<predictDiffReverse[i] << ";";
-            if(predictDiffReverse[i] > 0){
-                predict[n] += predictDiffReverse[i];
+            predictAvg = predictAvg/(double)bestPredict.size();
+            int aftdeal = (int)std::round(predictAvg + tempdataArray[tempdataArray.size() - 7]);  
+            if (aftdeal < 0)
+            {
+                aftdeal = 0;
             }
-        }
-        std::cout<< std::cout;
-        predict[n] = (int)(predict[n] + 0.5);
+            if(i<dataArrayNums-aheaddays)
+            {
+                if(abs(aftdeal - dataArray[aheaddays+i]) > 10)
+                {
+                    tempdataArray.push_back(double(aftdeal));
+
+                }
+                else
+                {
+                    tempdataArray.push_back(dataArray[aheaddays+i]);
+                }   
+            }
+            else
+            {
+                tempdataArray.push_back(double(aftdeal));
+            }
+            if(tempdataArray.size() > dataArrayNums)
+            {
+                weekPredict += aftdeal;
+            }
+            
+            //weekPredict += aftdeal;
+            //dataArray.push_back(double(aftdeal));      
+        }   
+        predict[n] = weekPredict;
     }
 }
-
